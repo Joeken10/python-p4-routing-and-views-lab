56e6e50404693575cc935ef0f0dd41cbffba3c19
Joeken10 <joekentafir@gmail.com>
2024-09-30 20:49:08 +0300

    first commit

--- server/app.py
+++ server/app.py
@@ -6,17 +6,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Operations with Flask Routing and Views</h1>'
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
 
 @app.route('/print/<string:param>')
 def print_string(param):
     print(param) 
     return param
 
-@app.route('/count/<int:param>')
-def count(param):
-    count_str = "\n".join(str(i) for i in range(param + 1))
-    return count_str.replace("\n", "<br>") 
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    return '\n'.join(str(i) for i in range(parameter)) + '\n'
+
 
 @app.route('/math/<int:num1>/<string:operation>/<int:num2>')
 def math(num1, operation, num2):
